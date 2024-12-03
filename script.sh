0d53455e75a201a8a710cdbd2e35a619687e75cd
PietroMC <xdexco@gmail.com>
2024-12-03 19:23:04 +0100

    24 trainings launched, waiting for results

---
+++ script.sh
@@ -0,0 +1,36 @@
+
+
+python3 /work/project/train.py --dataset imagenette --model resnet18 --epochs 200 --lr 0.0001 
+python3 /work/project/train.py --dataset imagenette --model resnet34 --epochs 200 --lr 0.0001
+python3 /work/project/train.py --dataset imagenette --model resnet50 --epochs 200 --lr 0.0001
+
+python3 /work/project/train.py --dataset imagenette --model resnet18 --epochs 200 --lr 0.0001 --pretrained
+python3 /work/project/train.py --dataset imagenette --model resnet34 --epochs 200 --lr 0.0001 --pretrained
+python3 /work/project/train.py --dataset imagenette --model resnet50 --epochs 200 --lr 0.0001 --pretrained
+
+
+python3 /work/project/train.py --dataset cifar10 --model resnet18 --epochs 200 --lr 0.0001
+python3 /work/project/train.py --dataset cifar10 --model resnet34 --epochs 200 --lr 0.0001
+python3 /work/project/train.py --dataset cifar10 --model resnet50 --epochs 200 --lr 0.0001
+
+python3 /work/project/train.py --dataset cifar10 --model resnet18 --epochs 200 --lr 0.0001 --pretrained
+python3 /work/project/train.py --dataset cifar10 --model resnet34 --epochs 200 --lr 0.0001 --pretrained
+python3 /work/project/train.py --dataset cifar10 --model resnet50 --epochs 200 --lr 0.0001 --pretrained
+
+
+python3 /work/project/train.py --dataset cifar100 --model resnet18 --epochs 200 --lr 0.0001
+python3 /work/project/train.py --dataset cifar100 --model resnet34 --epochs 200 --lr 0.0001
+python3 /work/project/train.py --dataset cifar100 --model resnet50 --epochs 200 --lr 0.0001
+
+python3 /work/project/train.py --dataset cifar100 --model resnet18 --epochs 200 --lr 0.0001 --pretrained
+python3 /work/project/train.py --dataset cifar100 --model resnet34 --epochs 200 --lr 0.0001 --pretrained
+python3 /work/project/train.py --dataset cifar100 --model resnet50 --epochs 200 --lr 0.0001 --pretrained
+
+
+python3 /work/project/train.py --dataset caltech101 --model resnet18 --epochs 200 --lr 0.0001
+python3 /work/project/train.py --dataset caltech101 --model resnet34 --epochs 200 --lr 0.0001
+python3 /work/project/train.py --dataset caltech101 --model resnet50 --epochs 200 --lr 0.0001
+
+python3 /work/project/train.py --dataset caltech256 --model resnet18 --epochs 200 --lr 0.0001 --pretrained
+python3 /work/project/train.py --dataset caltech256 --model resnet34 --epochs 200 --lr 0.0001 --pretrained
+python3 /work/project/train.py --dataset caltech256 --model resnet50 --epochs 200 --lr 0.0001 --pretrained
